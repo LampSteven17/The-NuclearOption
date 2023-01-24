8975c4940c0078dab4df6157aa2cc95961c9c9f2
Steven Lamp <42517762+LampSteven17@users.noreply.github.com>
2023-01-24 13:39:17 -0500

    Update theNuclearReset.sh

--- theNuclearReset.sh
+++ theNuclearReset.sh
@@ -24,15 +24,7 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update 
 sudo apt install fish
 
-# OH MY FISH INSTALLATION: https://github.com/oh-my-fish/oh-my-fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
-# BASS INSTALLATION: https://github.com/edc/bass
-omf install bass
-
-# SET FISH AS DEFAULT TERMINAL 
-echo /usr/bin/fish | sudo tee -a /etc/shells  
-chsh -s /usr/bin/fish  
 
 #OLD PYTHON VERSION DEPENDENCIES
 sudo add-apt-repository ppa:deadsnakes/ppa
@@ -51,4 +43,15 @@ sudo apt install curl
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
 sudo python2 get-pip.py
 
-# CUDA 10.2 Installation 
+# CUDA 11.6 Installation 
+wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pin
+sudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600
+wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-wsl-ubuntu-11-6-local_11.6.0-1_amd64.deb
+sudo dpkg -i cuda-repo-wsl-ubuntu-11-6-local_11.6.0-1_amd64.deb
+sudo apt-key add /var/cuda-repo-wsl-ubuntu-11-6-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
+
+#EXPORT TO ~/.bashrc
+echo 'export PATH="/usr/local/cuda-8.0/bin:$PATH"' >> ~/.bashrc
+echo 'export LD_LIBRARY_PATH="/usr/local/cuda-8.0/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc 
